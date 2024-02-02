ea097d5168850957fa33d16f5401e7a7834f3a10
Ric Evans <ric@evans-jacquez.com>
2024-02-02 14:02:25 -0600

    add functions to new package

---
+++ py_version_parser/__init__.py
@@ -0,0 +1,23 @@
+"""Public init."""
+
+from .py_version_parser import get_latest_py3_release, list_py3_releases
+
+__all__ = [
+    "get_latest_py3_release",
+    "list_py3_releases",
+]
+
+# version is a human-readable version number.
+
+# version_info is a four-tuple for programmatic comparison. The first
+# three numbers are the components of the version number. The fourth
+# is zero for an official release, positive for a development branch,
+# or negative for a release candidate or beta (after the base version
+# number has been incremented)
+__version__ = "2.1.0"
+version_info = (
+    int(__version__.split(".")[0]),
+    int(__version__.split(".")[1]),
+    int(__version__.split(".")[2]),
+    0,
+)
